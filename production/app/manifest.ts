2c4c613a65b300462eccf165025fc31278b1c811
capquangfptminhlh <capquangfpt.minhlh@gmail.com>
2026-09-11 15:14:33 +0700

    feat: add web app manifest

---
+++ production/app/manifest.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from 'next'
+export default function manifest():MetadataRoute.Manifest{return{name:'ChoVot Pickleball',short_name:'ChoVot',description:'Marketplace mua bán vợt pickleball có xác thực',start_url:'/',display:'standalone',background_color:'#ffffff',theme_color:'#087a4b',icons:[{src:'/brand/icon.svg',sizes:'any',type:'image/svg+xml'}]}}
