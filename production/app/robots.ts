87e64a5d83827170ea690b54bf07c29bcccb302e
capquangfptminhlh <capquangfpt.minhlh@gmail.com>
2026-09-11 15:07:39 +0700

    feat: add robots metadata route

---
+++ production/app/robots.ts
@@ -0,0 +1,4 @@
+import type { MetadataRoute } from 'next'
+import { site } from '@/lib/site'
+
+export default function robots():MetadataRoute.Robots{return{rules:[{userAgent:'*',allow:'/',disallow:['/api/','/tai-khoan/','/quan-tri/']}],sitemap:`${site.url}/sitemap.xml`,host:site.url}}
